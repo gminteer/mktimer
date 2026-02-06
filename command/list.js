cd337123db0d7d973a5dd0d574b3c0cd5f3dc672
Greg Minteer <gminteer@gmail.com>
2026-02-05 20:49:01 -0500

    feat: rough proof of concept for list command

---
+++ command/list.js
@@ -0,0 +1,24 @@
+import {Command} from 'commander';
+
+import addGlobals from '../lib/global-options.js';
+
+export function makeListAction($) {
+  return function (filter, options, command) {
+    const out = $`systemctl --user --output json list-timers ${filter}`;
+    if (!out.ok) command.error(out.stderr);
+    const timers = JSON.parse(out);
+    for (const timer of timers) {
+      console.debug(timer);
+      const timestamp = new Date(timer.next / 1000).toLocaleString();
+      console.debug(timestamp);
+    }
+  };
+}
+
+export default function makeListCommand() {
+  return addGlobals(new Command())
+    .name('list')
+    .alias('ls')
+    .description('lists timers')
+    .argument('[filter]', 'only display timers matching [filter]');
+}
