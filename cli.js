4e22a15ccb0e08609bf533ef6fd58a4f623daf10
Greg Minteer <gminteer@gmail.com>
2026-02-11 02:46:52 -0500

    🌈 style(cli): cleanup cli.js

--- cli.js
+++ cli.js
@@ -5,17 +5,11 @@ import {$ as $_} from 'zx';
 const $ = $_({nothrow: true, quiet: true, sync: true});
 
 import addListCommand, {makeListAction} from './command/list.js';
-// import makeListCommand, {makeListAction} from './command/list.js';
 import addRunCommand, {makeRunAction} from './command/run.js';
 import program from './lib/common.js';
 import {makeParseExecStart, makeParseTimer} from './lib/parsers.js';
 import pkg from './package.json' with {type: 'json'};
 
-// build list command
-// const listCommand = makeListCommand();
-// listCommand.action(makeListAction($));
-
-// build run command
 const parseExecStart = makeParseExecStart(accessSync, realpathSync);
 const parseTimer = makeParseTimer($);
 
